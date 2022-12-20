f32a028079171bd75830e99abbafb65b0b2ad691
Sefatullah Omar <sefaullahomar1@gmail.com>
2022-12-19 23:33:47 -0800

    tested server and ejs engine

---
+++ README.md
@@ -0,0 +1,3 @@
+//PIZZA ORDER TRACKING SYSTEM USING EJS ENGINE
+
+- ** /Resources used for all the html templates on user side **
--- index.js
+++ index.js
@@ -9,16 +9,30 @@ const path = require('path');
 
 PORT = process.env.PORT || 5000;
 
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+
+
 //test run
 app.get("/", (req, res) => {
-    res.send("Hi there!");
+    res.render("home");// views/render home.ejs 
 })
 
 
 //SET TEMPLATE ENGINE..
 app.use(expressJsLayout);
-app.set("views", path.join(__dirname,"resources/views")) // to find views
-app.set("view engine")
+app.set("views", path.join(__dirname,"/resources/views")) // to find views
+app.set("view engine", "ejs")
+
+
+
+
+
+
+
+
 
 app.listen(PORT, ()=>{
     console.log('listening on port', PORT)
