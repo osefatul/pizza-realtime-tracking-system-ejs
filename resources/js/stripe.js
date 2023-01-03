b343354dfbfc9f9da0518f48ae4a58b91e4ab983
Sefatullah Omar <sefaullahomar1@gmail.com>
2023-01-02 20:26:17 -0800

    done

--- resources/js/stripe.js
+++ resources/js/stripe.js
@@ -18,17 +18,16 @@ export async function initStripe() {
         //This will prevent the form to be submitted from cart.ejs action and method attributes, so we can perform below stuff.
         e.preventDefault();
 
-        
+
         let formData =  new FormData(paymentForm);
         let formObject = {};
 
         console.log("Hello from stripe")
         console.log(formData)
 
-        // for(let [key, value] of formData.entries()){
-        //     formObject[key] = value;
-        // }
-
+        for(let [key, value] of formData.entries()) {
+            console.log(key, value)
+        }
         // if(!card){
         //     placeOrder(formObject)
         // }
